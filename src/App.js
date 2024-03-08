a31e8161fea9c0afe21a12f14c601004f5292698
Naveen-Varadharaj <naveenvaradharaj28@gmail.com>
2024-03-08 13:43:47 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { useState } from 'react';
-import './App.css'; // You may need to create this file for styling
-import logo from './image/logo.gif'
-const App = () => {
-  const [clickPosition, setClickPosition] = useState({ x: 12, y: 585 });
+import './App.css';
+import logo from './image/logo.gif';
 
+const App = () => {
+  const [clickPosition, setClickPosition] = useState({ x: 112, y: 185 });
+  const[sc,setSc]=useState(-1)
   const handleClick = (e) => {
     const { clientX, clientY } = e;
-    // console.log(clientX);
-    // console.log(clientY);
+    if(clickPosition.x<clientX && clickPosition.y<clientY){
+      setSc(-1)
+    }
+    else{
+      setSc(1);
+    }
     setClickPosition({ x: clientX, y: clientY });
   };
+
+  const calculateRotationAngle = (clickX, clickY) => {
+    const imageX = clickPosition.x;
+    const imageY = clickPosition.y;
+    const angleRad = Math.atan2(clickY - imageY, clickX - imageX);
+    return angleRad * (180 / Math.PI) ; 
+  };
+
   return (
     <div className="App" onClick={handleClick}>
       <img
@@ -18,10 +31,11 @@ const App = () => {
         style={{
           position: 'absolute',
           left: clickPosition.x,
-          top: clickPosition.y,         
+          top: clickPosition.y,
           transition:'left 10s, top 10s',
-          width:'100px',
-          height:'100px'
+          transform: `translate(-50%, -50%) rotate(${calculateRotationAngle(clickPosition.x, clickPosition.y)}deg) scaleX(${sc})`, 
+          width: '100px',
+          height: '100px'
         }}
       />
     </div>
